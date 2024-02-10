29f6faca54913caad12c7114a03bc84f169f0183
Gayathri Devi Kuppusamy <gayathri.kuppusamy@itp.objectfrontier.com>
2024-02-10 14:08:11 +0530

    first commit

---
+++ src/main/java/com/abp/repository/MessageRepo.java
@@ -0,0 +1,10 @@
+package com.abp.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+import com.abp.bean.Message;
+
+
+public interface MessageRepo extends CrudRepository<Message, Integer> {
+}
